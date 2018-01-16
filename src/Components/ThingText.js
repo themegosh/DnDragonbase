9e8a92a8b9be0578033efb09702bd188ca53f17d
Doug Mathew <dmathew@office.consulnet.com>
2018-01-16 14:07:29 -0500

    items work now

--- src/Components/ThingText.js
+++ src/Components/ThingText.js
@@ -3,21 +3,16 @@ import React, {Component} from 'react';
 class ThingText extends Component {
     render() {
         let text = null
-        if (Array.isArray(this.props)){
-            text = this.props.map((thing, key) => {
-                console.log(thing);
-                <div key={key}>{thing}</div>
+        if (Array.isArray(this.props.text)){
+            text = this.props.text.map((thing, key) => {
+                return <p key={key}>{thing}</p>
             })
         } else {
-            text = <div>{this.props}</div> 
+            return <p>{this.props.text}</p>
         }
-            console.log(this.props)
 
         return (
-            this.props.map((thing, key) => {
-                console.log(thing);
-                <div key={key}>{thing}</div>
-            })
+            text
         );
     }
 }
--- src/Containers/Item/Item.js
+++ src/Containers/Item/Item.js
@@ -3,31 +3,45 @@ import ThingText from '../../Components/ThingText'
 import PrettyObj from '../../Components/PrettyObj'
 import Compendium from '../../Helpers/Compendium'
 
-
 class Item extends Component {
-    constructor(props){
+    constructor(props) {
         super();
         this.state = {
             item: {},
             found: false
         }
 
-        Compendium.Items.forEach(anItem => {
-            if (anItem.name === props.match.params.name){
-                this.state.item = anItem;
-                this.state.found = true;
-            }
-        })
-
+        Compendium
+            .Items
+            .forEach(anItem => {
+                if (anItem.name === props.match.params.name) {
+                    this.state.item = anItem;
+                    this.state.found = true;
+                }
+            })
     }
 
     render() {
 
-        return ( 
-            <div className="Item">
-                <h1>{this.state.item.name}</h1>
-                <ThingText {...this.state.item.text}></ThingText>
-                <hr/>
+        var itemStats = Object.keys(this.state.item).map(
+            key => {
+                if (key !== 'text')
+                    return <div key={key}><strong>{key}:</strong> {this.state.item[key]}</div>
+            }
+        )
+
+        return (
+            <div>
+                <div className='container'>
+                    <div className="Item">
+                        <h1>{this.state.item.name}</h1>
+                        <hr/>
+                        {itemStats}
+                        <hr/>
+                        <ThingText text={this.state.item.text}></ThingText>
+                        <hr/>
+                    </div>
+                </div>
                 <PrettyObj {...this.state.item}></PrettyObj>
             </div>
         );
