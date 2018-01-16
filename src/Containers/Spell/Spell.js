96b7d7e68dfe443e300d07250031f8ffce09aae5
Doug Mathew <dmathew@office.consulnet.com>
2018-01-16 14:34:30 -0500

    spells!

--- src/Containers/Spell/Spell.js
+++ src/Containers/Spell/Spell.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import PrettyObj from '../../Components/PrettyObj'
+import ThingText from '../../Components/ThingText'
 import Compendium from '../../Helpers/Compendium'
 
 class Spell extends Component {
@@ -21,9 +22,22 @@ class Spell extends Component {
 
     render() {
 
+        var spellStats = Object.keys(this.state.spell).map(
+            key => {
+                if (key !== 'text')
+                    return <div key={key}><strong className='thing-summary-name'>{key}:</strong> {this.state.spell[key]}</div>
+            }
+        )
+
         return ( 
-            <div className="Spell">
-                <h1>{this.state.spell.name}</h1>
+            <div className='container'>
+                <div className="Spell">
+                    <h1>{this.state.spell.name}</h1>
+                    <hr/>
+                    {spellStats}
+                    <hr/>
+                    <ThingText text={this.state.spell.text}></ThingText>
+                </div>
                 <PrettyObj {...this.state.spell}></PrettyObj>
             </div>
         );
