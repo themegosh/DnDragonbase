65d9fd6ce5ca861720f93358c10b7b87561b96c2
Doug Mathew <mathewdf@gmail.com>
2018-01-28 03:54:29 -0500

    some work on class

--- src/Containers/Class/Class.js
+++ src/Containers/Class/Class.js
@@ -1,39 +1,84 @@
-import React, {Component} from 'react'
-import PrettyObj from '../../Components/PrettyObj'
-import Compendium from '../../Helpers/Compendium'
+import React, { Component } from "react";
+import PrettyObj from "../../Components/PrettyObj";
+import Compendium from "../../Helpers/Compendium";
 
 class Class extends Component {
-    constructor(props){
+    constructor(props) {
         super();
         this.state = {
-            theClass: {},
-        }
+            theClass: {}
+        };
     }
 
     getClass(name) {
-        Compendium
-            .Classes
-            .forEach(aClass => {
-                if (aClass.name === name) {
-                    this.setState({theClass: aClass})
-                }
-            })
+        Compendium.Classes.forEach(aClass => {
+            if (aClass.name === name) {
+                console.log(aClass);
+                this.setState({ theClass: aClass });
+            }
+        });
     }
 
     componentWillMount() {
-        this.getClass(this.props.match.params.name)
+        this.getClass(this.props.match.params.name);
     }
 
     componentWillReceiveProps(props) {
-        this.getClass(props.match.params.name)
+        this.getClass(props.match.params.name);
+    }
+
+    getClassChart() {
+        var rows = [];
+        var maxClassLvl = Object.keys(this.state.theClass.levels).length;
+        for (let lvlIndex = 1; lvlIndex <= 20; lvlIndex++) {
+            var features = this.state.theClass.levels[lvlIndex].features.map(
+                (aFeature, key) => {
+                    return <span>{aFeature.name} | </span>;
+                }
+            );
+            var slots = [];
+            for (var slotIndex = 1; slotIndex <= this.state.theClass.levels[maxClassLvl].slots.length; slotIndex++) {
+                if (this.state.theClass.levels[lvlIndex].slots[slotIndex]) {
+                    slots.push(
+                        <td key={slotIndex}>
+                            {this.state.theClass.levels[lvlIndex].slots[slotIndex]}
+                        </td>
+                    );
+                } else {
+                    slots.push(<td key={slotIndex}>-</td>);
+                }
+            }
+            console.log(features);
+            rows.push(
+                <tr key={lvlIndex}>
+                    <td>
+                        <h2>{lvlIndex}</h2>
+                    </td>
+                    <td>{features}</td>
+                    {slots}
+                </tr>
+            );
+        }
+        return (
+            <table className="table table-striped">
+            <thead>
+                <tr>
+                    <td>Level</td>
+                    <td>Features</td>
+                    
+                </tr>
+            </thead>
+                <tbody>{rows}</tbody>
+            </table>
+        );
     }
 
     render() {
-
-        return ( 
+        return (
             <div className="Class">
                 <h1>{this.state.theClass.name}</h1>
-                <PrettyObj {...this.state.theClass}></PrettyObj>
+                {this.getClassChart()}
+                <PrettyObj {...this.state.theClass} />
             </div>
         );
     }
