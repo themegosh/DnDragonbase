60cb9c02247629beb20a10f78bdb9491b1a35049
Doug Mathew <mathewdf@gmail.com>
2018-01-28 04:33:36 -0500

    make it stateless

--- src/Components/OrdinalSuffix.js
+++ src/Components/OrdinalSuffix.js
@@ -1,29 +1,23 @@
-import { Component } from "react";
 
-class OrdinalSuffix extends Component {
-    //-bonus -ability score -skills
-
-    render() {
-        if (!this.props.num) {
-            return null;
-        }
-
-        var i = this.props.num;
-
-        var j = i % 10,
-            k = i % 100;
-        if (j === 1 && k !== 11) {
-            return i + "st";
-        }
-        if (j === 2 && k !== 12) {
-            return i + "nd";
-        }
-        if (j === 3 && k !== 13) {
-            return i + "rd";
-        }
-        return i + "th";
+const OrdinalSuffix = props => {
+    if (!props.num) {
+        return null;
     }
-}
+
+    var i = props.num;
+
+    var j = i % 10,
+        k = i % 100;
+    if (j === 1 && k !== 11) {
+        return i + "st";
+    }
+    if (j === 2 && k !== 12) {
+        return i + "nd";
+    }
+    if (j === 3 && k !== 13) {
+        return i + "rd";
+    }
+    return i + "th";
+};
 
 export default OrdinalSuffix;
-
