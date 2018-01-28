292c04c6073ec980b125f34865bf44d08865e855
Doug Mathew <mathewdf@gmail.com>
2018-01-28 04:37:41 -0500

    change to HashRouter for github pages 404 error

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Link, Switch } from "react-router-dom";
 
 import NotFound from "./NotFound";
 import Home from "./Home";
